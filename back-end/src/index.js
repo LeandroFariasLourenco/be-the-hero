646c854352c3a6de5e977ee130b15d322c15bd13
Leandro <leandro.farias01@outlook.com>
2020-03-28 11:55:05 -0300

    feat(back-end):Implementing validations

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express'),
     app = express(),
     routes = require('./routes'),
-    cors = require('cors');
+    cors = require('cors'),
+    { errors } = require('celebrate'); 
 
 app.use(cors());
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.listen(3333);
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,18 +1,38 @@
 const express = require('express'),
-      routes = express.Router(),
-      ongController = require('./controllers/T_HERO_ONGS'),
-      ongProfile = require('./controllers/ONG_Profile'),
-      ongSession = require('./controllers/ONG_Session'),
-      incidentController = require('./controllers/T_HERO_INCIDENTS');
+  routes = express.Router(),
+  ongController = require('./controllers/T_HERO_ONGS'),
+  ongProfile = require('./controllers/ONG_Profile'),
+  ongSession = require('./controllers/ONG_Session'),
+  incidentController = require('./controllers/T_HERO_INCIDENTS'),
+  { celebrate, Segments, Joi } = require('celebrate');
 
-routes.post('/ongs', ongController.post);
-routes.post('/session', ongSession.post);
+
 routes.get('/ongs', ongController.get);
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}) , incidentController.get);
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.required()
+  }).unknown()
+}) , ongProfile.get);
 
-routes.get('/profile', ongProfile.get);
 
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    nome: Joi.string().required(),
+    email: Joi.string().required().email(),
+    telefone: Joi.string().required().max(15).min(10),
+    cidade: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), ongController.post);
+routes.post('/session', ongSession.post);
 routes.post('/incidents', incidentController.post);
-routes.get('/incidents', incidentController.get);
+
+
 routes.delete('/incidents/:id', incidentController.delete);
 
 module.exports = routes;
